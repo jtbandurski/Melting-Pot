91d49b5c03d9e78fe3a13d69f4ae5e1660a88c7f
jtbandurski <jakub.t.bandrski@gmail.com>
2024-04-13 14:48:37 +0200

    vm setup script

--- vm_setup1.sh
+++
@@ -1,6 +0,0 @@
-export WANDB_API_KEY=6ee6df69a17811e60b402f0f1564bd11ec4fd2eb
-sudo apt-get update -y
-sudo apt-get install git -y
-git clone https://github.com/jtbandurski/Melting-Pot
-cd Melting-Pot
-git push
