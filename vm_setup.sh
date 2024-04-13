fd88ffdf52e3e529f732d988bfd29bf1825c5c9b
jtbandurski <jakub.t.bandrski@gmail.com>
2024-04-13 14:49:52 +0200

    vm setup script

--- vm_setup.sh
+++ vm_setup.sh
@@ -1,6 +1,5 @@
 export WANDB_API_KEY=6ee6df69a17811e60b402f0f1564bd11ec4fd2eb
 sudo apt-get update -y
-sudo apt-get install git -y
 sudo apt install python3.10-virtualenv -y
 python3 -m venv .venv
 source .venv/bin/activate
